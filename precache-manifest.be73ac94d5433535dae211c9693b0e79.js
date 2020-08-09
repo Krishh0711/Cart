c745a7f0c01d608f26be54bf1368f1f210d1fc39
Krishh258 <krishnanshu33@gmail.com>
2020-08-09 14:02:32 +0530

    Updates

---
+++ precache-manifest.be73ac94d5433535dae211c9693b0e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e942fcaba7e98533593e1f4462618e",
+    "url": "/Cart/index.html"
+  },
+  {
+    "revision": "f8f84c6c2069f38de517",
+    "url": "/Cart/static/css/main.85421fbf.chunk.css"
+  },
+  {
+    "revision": "1a17ce069dd10e8a4987",
+    "url": "/Cart/static/js/2.71d4ea96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cart/static/js/2.71d4ea96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f84c6c2069f38de517",
+    "url": "/Cart/static/js/main.103d9d50.chunk.js"
+  },
+  {
+    "revision": "a469069d01a9da7162f6",
+    "url": "/Cart/static/js/runtime-main.6525e319.js"
+  }
+]);
